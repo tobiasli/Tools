8bfee80bb2607c61862b50cb83e03d36464e4de0
Tobias Litherland <Tobias.Litherland@statkraft.com>
2017-02-05 17:18:28 +0100

    Added cleaned tregex /w test from statkraft project

--- tests/test_tregex.py
+++ tests/test_tregex.py
@@ -1,21 +1,13 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-#-------------------------------------------------------------------------------
-# Name:        test_units
-# Purpose:      Test the components of the units module.
-#
-# Author:      Tobias
-#
-# Created:     19.10.2015
-# Copyright:   (c) Tobias 2015
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
+"""
+Tests copied and structured from https://github.com/tobiasli/Tools
+"""
+
 import unittest
 import sys
 import os
 import time
 
-sys.path = [os.path.split(os.path.split(os.path.realpath(__file__))[0])[0] + os.path.sep] + sys.path
+from hydrology_toolbox.text_tools import tregex
 
 FIND_TEST_CASES = [
         ('nisse \w+ \d fjell','nisse fjes 3 fjell'),
@@ -28,28 +20,32 @@ GROUP_TEST_CASES = [
         ('(nisse) (\w+) (\d) (fjell)','nisse fjes 3 fjell', ('nisse', 'fjes', '3', 'fjell')),
         ]
 
-class TestTregexModule(unittest.TestCase):
-    def test_tregex_find(self):
-        import tregex
+SEARCH_LIST = ['Stavern', 'Larvik', 'Sandefjord', 'Tønsberg', 'Åsgårdstrand', 'Horten', 'Holmestrand']
+SEARCH_MAPPING = {'strand': ['Holmestrand', 'Åsgårdstrand', 'Stavern'], 'naberg': ['Tønsberg'], 'larv': ['Larvik']}
 
+
+class TestTregexModule(unittest.TestCase):
+    def test_tregex_match(self):
         for pattern, candidate in FIND_TEST_CASES:
-            self.assertTrue(tregex.find(pattern, candidate), candidate)
+            self.assertTrue(tregex.match(pattern, candidate), candidate)
 
     def test_tregex_group(self):
-        import tregex
-
         for pattern, candidate, response in GROUP_TEST_CASES:
-            self.assertTrue(tregex.find(pattern, candidate), response)
+            self.assertTrue(tregex.group(pattern, candidate), response)
 
     def test_tregex_smart(self):
-        import tregex
+        for case, instance in zip(FIND_TEST_CASES, FIND_TEST_CASES_TYPES):
+            self.assertTrue(isinstance(tregex.smart(case[0], case[1])[0], instance))
 
-        for case, type in zip(FIND_TEST_CASES, FIND_TEST_CASES_TYPES):
-            self.assertTrue(isinstance(tregex.smart(case[0], case[1])[0], type))
+    def test_tregex_find(self):
+        for search, match in SEARCH_MAPPING.items():
+            result = tregex.find(search, SEARCH_LIST)
 
-def run():
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestTregexModule)
-    unittest.TextTestRunner(verbosity=2).run(suite)
+            self.assertTrue(len(result) > 0)
+            self.assertTrue(result[0] in match)
+
+        result = tregex.find_best('larvik', SEARCH_LIST, score_cutoff=1)
+        self.assertTrue(result in SEARCH_LIST)
 
 if __name__ == '__main__':
-    run()
+    unittest.main()
